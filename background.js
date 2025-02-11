edfc1a2137b5c2197ba4178e2de27557577e69bd
Ron Barrantes <ronbarrantes@gmail.com>
2025-02-11 14:07:00 -0600

    Add to bg.js

--- background.js
+++ background.js
@@ -1,5 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
+const extensions = "https://developer.chrome.com/docs/extensions";
+const webstore = "https://developer.chrome.com/docs/webstore";
+
+chrome.runtime.onInstalled.addListener(async (tab) => {
+  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+    const prevState = await chrome.actions.geBadgeText({ tabId: tab.id });
+    const nextState = prevState === "ON" ? "OFF" : "ON";
+
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+  }
 });
