1227617bf182c3c64cc3cec240a7c386f28e4fa2
Ron Barrantes <ronbarrantes@gmail.com>
2025-02-11 15:55:13 -0600

    Fix the service worker thingy

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
+});
+
 const extensions = "https://developer.chrome.com/docs/extensions";
 const webstore = "https://developer.chrome.com/docs/webstore";
 
-chrome.runtime.onInstalled.addListener(async (tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    const prevState = await chrome.actions.geBadgeText({ tabId: tab.id });
+    const prevState = await chrome.action.getBadgeTest({ tabId: tab.id });
     const nextState = prevState === "ON" ? "OFF" : "ON";
 
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
+    await chrome.action.setBadgeText({ tabId: tab.id, text: nextState });
   }
 });
